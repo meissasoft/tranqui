9d83d9cfc37760441233022220a9206ed70c389e
SulemanAwais <102269316+SulemanAwais@users.noreply.github.com>
2024-10-07 12:26:09 +0500

    Update consumers.py

--- tranqui/api/consumers.py
+++ tranqui/api/consumers.py
@@ -14,7 +14,7 @@ from openai import OpenAI, OpenAIError
 logger = logging.getLogger(__name__)
 client = OpenAI(api_key=settings.OPENAI_API_KEY)
 OPENAI_TOKEN_LIMIT = int(getattr(settings, "OPENAI_TOKEN_LIMIT", 5000))
-TOKEN_PER_WORD = getattr(settings.TOKEN_PER_WORD, "0.57")
+TOKEN_PER_WORD = int(getattr(settings.TOKEN_PER_WORD, "0.57"))
 
 
 async def get_user(username):
