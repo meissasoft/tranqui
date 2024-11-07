77e085030f05ec8e5b15794e07aaab0f239a905c
SulemanAwais <sulemanawais2001@gmail.com>
2024-11-07 18:04:01 +0500

    Update livekit_Assistant.py

--- tranqui/livekit_Assistant.py
+++ tranqui/livekit_Assistant.py
@@ -73,4 +73,4 @@ async def entrypoint(ctx: JobContext):
 
 
 if __name__ == "__main__":
-    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, agent_name="Tranqui Agent"))
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
