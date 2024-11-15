9dd634c667d88fd4eba778335dc563cfa4d29cad
SulemanAwais <sulemanawais2001@gmail.com>
2024-11-15 13:12:52 +0500

    Improved URLs

--- tranqui/api/urls.py
+++ tranqui/api/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
-    path('auth/register/', RegisterUserView.as_view(), name='register'),
-    path('auth/profile-update/', ProfileUpdateView.as_view(), name='profile_update'),
-    path('auth/login/', LoginView.as_view(), name='login'),
-    path('auth/verify-OTP/', VerifyOTPView.as_view(), name='send_confirmation_code'),
-    path('auth/verify-reset-code/', VerifyResetCodeView.as_view(), name='validate_reset_code'),
-    path('auth/reset-password/', ResetPasswordView.as_view(), name='reset_password'),
-    path('auth/google-sign-in/', GoogleSignInView.as_view(), name='google-sign-in'),
-    path('chats/', GetAllChatsView.as_view(), name='get_all_chats'),
-    path('chats/create-chat/', CreateChatView.as_view(), name='create-chat'),
-    path('get-livekit-token/', GetLiveKitToken.as_view(), name='get_livekit_token'),
+    # Auth URLS
+    path('auth/register/', RegisterUserView.as_view(), name='register_user'),
+    path('auth/profile/', ProfileUpdateView.as_view(), name='update_profile'),
+    path('auth/login/', LoginView.as_view(), name='user_login'),
+    path('auth/otp/verify/', VerifyOTPView.as_view(), name='verify_otp'),
+    # path('auth/otp/resend/', ResendOTPView.as_view(), name='resend_otp'),
+    path('auth/password/reset/verify-code/', VerifyResetCodeView.as_view(), name='verify_reset_code'),
+    path('auth/password/reset/', ResetPasswordView.as_view(), name='reset_password'),
+    path('auth/google-signin/', GoogleSignInView.as_view(), name='google_signin'),
+    # Chats URLs
+    path('chats/', GetAllChatsView.as_view(), name='list_chats'),
+    path('chats/create/', CreateChatView.as_view(), name='create_chat'),
+    # LiveKit Token
+    path('livekit/token/', GetLiveKitToken.as_view(), name='get_livekit_token'),
 ]
